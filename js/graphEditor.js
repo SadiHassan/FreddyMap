59a2d77943355bb7cdeb5c0646f41e825b9efa7f
Mallik Hassan <sadi.research@gmail.com>
2024-09-25 23:57:26 -0300

    2.graph editor (#9)
    * graphEditor init
    
    * graph editor ++
    
    ---------
    
    Co-authored-by: Mallik <mallik@Malliks-MacBook-Pro.local>

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,5 +4,42 @@ class GraphEditor {
         this.graph = graph;
 
         this.ctx = this.canvas.getContext("2d");
+
+        this.selected = null;
+        this.hovered = null;
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners() {
+        this.canvas.addEventListener("mousedown", (evt)=>{
+            console.log(evt);
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+
+            if (this.hovered) {
+                this.selected = this.hovered;
+                return;
+            }
+
+            this.graph.addPoint(mouse);
+            this.selected = mouse;
+        });
+
+        this.canvas.addEventListener("mousemove", (evt)=>{
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+        });
+    }
+
+    display() {
+        this.graph.draw(this.ctx);
+
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true });
+        }
+
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true });
+        }
     }
 }
